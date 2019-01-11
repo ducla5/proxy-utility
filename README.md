32f690566b0e640f99faa06f8acd7933fb8dfd2c
Le Anh Duc <ducla@lifull-tech.vn>
2019-01-11 17:18:05 +0700

    add multi threading

--- README.md
+++ README.md
@@ -12,12 +12,12 @@
   - -ob or --out-banned: File to write banned proxies to (banned). Default is banned.txt
   - -of or --out-good: File to write good proxies to (not banned). Default is good.txt
   - -ef or --error-file: File to store errored out proxies. Default is error.txt
-  - -t or --timeout: 'Timeout for requests to servers. Default is 5s
+  - -t or --timeout: Timeout for requests to servers. Default is 5s
+  - -th or -- thread: Number of thread will run. Default is 10 
 
 ## Customize servers to check:
  - Replace server you need to work with in file servers.txt. One per line
  - 
 ## TODO: 
- - Using multi threads to run faster
  - Detect location and anonymity of proxy
  - Sort result by response, speed ...
--- proxy_checker.py
+++ proxy_checker.py
@@ -1,5 +1,12 @@
 import argparse
 import requests
+import threading
+from threading import Thread
+import time
+
+good = []
+error = []
+banned = []
 
 
 def main():
@@ -18,6 +25,8 @@ def main():
                         help='File to store errored out proxies')
     parser.add_argument('-t', '--timeout', default=5,
                         help='Timeout for requests to servers.')
+    parser.add_argument('-th', '--thread', default=10,
+                        help='Number of thread')
     args = parser.parse_args()
 
     servers = []
@@ -26,25 +35,36 @@ def main():
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
-
-    with open(args.input_file, 'r+') as f, open(args.out_banned, 'w+') as banned, open(args.out_good, 'w+') as good, open(args.error_file, 'w+') as error:
+    th = []
+    with open(args.input_file, 'r+') as f:
         proxies = f.read().splitlines()
-        for p in proxies:
-            # TODO: Multi thread to faster check
-            log('Attempting to check proxy {}...'.format(p))
-            pr = {'http': p, 'https': p}
-            flag = check_proxy(p, pr, servers, headers)
-            if flag == 0:
-                good.write('{}\n'.format(p))
-            elif flag == 1:
-                banned.write('{}\n'.format(p))
-            else:
-                error.write('{}\n'.format(p))
 
+        while proxies.__len__() > 0:
+            if threading.active_count() < int(args.thread):
+                p = proxies.pop()
+                log('Attempting to check proxy {}...'.format(p))
+                pr = {'http': p, 'https': p}
+                t = Thread(
+                    target=check_proxy, args=(p, pr, servers, headers))
+                t.start()
+                th.append(t)
+            else:
+                time.sleep(1)
         f.close()
-        good.close()
-        banned.close()
-        error.close()
+    for thread in th:
+        thread.join()
+
+    with open(args.out_good, 'w') as f:
+        for item in good:
+            f.write("%s\n" % item)
+
+    with open(args.out_banned, 'w') as f:
+        for item in banned:
+            f.write("%s\n" % item)
+
+    with open(args.error_file, 'w') as f:
+        for item in error:
+            f.write("%s\n" % item)
 
 
 def log(message, char='+'):
@@ -74,15 +94,20 @@ def check_proxy(p, proxy, servers, header):
                     '{}, 403 Forbidden, proxy is banned on : {}.'.format(p, server))
                 flag = 1
         except requests.exceptions.Timeout:
-            verbose_log('{}, Timed out after {} seconds.'.format(
-                p, args.timeout))
+            verbose_log(
+                '{}, Timed out after {} seconds.'.format(p, args.timeout))
         except requests.exceptions.RequestException as e:
             verbose_log('{pr}, Unable to connect to the proxy {pr}, or timed out. Make sure to add https://, and the port.'.format(
                 pr=p))
             log('requestsexception: ' + str(e), '!')
         if flag != 0:
             break
-    return flag
+    if flag == 0:
+        good.append(p)
+    elif flag == 1:
+        banned.append(p)
+    else:
+        error.append(p)
 
 
 if __name__ == '__main__':
